234d2685503bff46983ca2d986acb5d388065941
Trivium-bv <trivium.bv@gmail.com>
2025-02-13 11:58:02 +0300

    TodolistItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import './App.css'
+import { TodolistItem } from './TodolistItem'
 
-function App() {
+export const App = () => {
   return (
       <div className="app">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
+        <TodolistItem title="What to learn"/>
+        <TodolistItem title="Songs"/>
+        <TodolistItem title="Games"/>
+        <TodolistItem title="Films"/>
       </div>
   )
 }
