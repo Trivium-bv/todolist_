84f340a4cc82be28a947a4cbc0e7c56b7c9ed99f
Trivium-bv <trivium.bv@gmail.com>
2025-02-13 15:13:01 +0300

    tasks.map

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,22 @@ export const App = () => {
   const task1: Task[] = [
     { id: 1, title: "React", isDone: false },
     { id: 2, title: "JS", isDone: true },
-    { id: 1, title: "Redux", isDone: false }
+    { id: 3, title: "Redux", isDone: false }
   ]
   const task2: Task[] = [
     { id: 1, title: "World of WarCraft", isDone: false },
     { id: 2, title: "Half Life 2", isDone: true },
-    { id: 1, title: "Dota 2", isDone: true }
+    { id: 3, title: "Dota 2", isDone: true },
+    { id: 4, title: "Far Cry", isDone: true }
   ]
 
   return (
+    <>
     <div className="app">
       <TodolistItem title="What to learn" tasks={task1} date={"11.02.2025"}/>
       <TodolistItem title="Games" tasks={task2} />
     </div>
+    </>
   )
 }
 
