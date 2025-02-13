4dd79e896b2a974eaccb9e67b13f88f8f184bc6e
Trivium-bv <trivium.bv@gmail.com>
2025-02-13 13:10:45 +0300

    tasks props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import './App.css'
 import { TodolistItem } from './TodolistItem'
 
+export type Task = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
 export const App = () => {
+
+  const task1: Task[] = [
+    { id: 1, title: "React", isDone: false },
+    { id: 2, title: "JS", isDone: true },
+    { id: 1, title: "Redux", isDone: false }
+  ]
+  const task2: Task[] = [
+    { id: 1, title: "World of WarCraft", isDone: false },
+    { id: 2, title: "Half Life 2", isDone: true },
+    { id: 1, title: "Dota 2", isDone: true }
+  ]
+
   return (
-      <div className="app">
-        <TodolistItem title="What to learn"/>
-        <TodolistItem title="Songs"/>
-        <TodolistItem title="Games"/>
-        <TodolistItem title="Films"/>
-      </div>
+    <div className="app">
+      <TodolistItem title="What to learn" tasks={task1} date={"11.02.2025"}/>
+      <TodolistItem title="Games" tasks={task2} />
+    </div>
   )
 }
 
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,30 +1,38 @@
+import { Task } from './App'
 import './App.css'
 
-export const TodolistItem = (props: any) => {
+type Props = {
+  title: string
+  tasks: Task[]
+  date?: string
+}
+
+export const TodolistItem = ({ title, tasks, date }: Props) => {
   return (
-        <div>
-          <h3>{props.title}</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
+    <div>
+      <h3>{title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type="checkbox" checked={tasks[0].isDone} /> <span>{tasks[0].title}</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={tasks[1].isDone} /> <span>{tasks[1].title}</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={tasks[2].isDone} /> <span>{tasks[2].title}</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+      <div>{date}</div>
+    </div>
   )
 }
 
