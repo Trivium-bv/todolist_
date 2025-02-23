02e76e9a01d1d5fcb1cd224eaa69debb64d5a05e
Trivium-bv <trivium.bv@gmail.com>
2025-02-23 19:16:47 +0300

    add deleteTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 import './App.css'
-import { TodolistItem } from './TodolistItem'
+import {TodolistItem} from './TodolistItem'
+import {useState} from 'react';
 
 export type Task = {
-  id: number
-  title: string
-  isDone: boolean
+    id: number
+    title: string
+    isDone: boolean
 }
 
 export const App = () => {
-
-  const task1: Task[] = [
-    { id: 1, title: "React", isDone: false },
-    { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "Redux", isDone: false }
-  ]
-  const task2: Task[] = []
-
-  return (
-    <>
-    <div className="app">
-      <TodolistItem title="What to learn" tasks={task1} date={"11.02.2025"}/>
-      <TodolistItem title="Games" tasks={task2} />
-    </div>
-    </>
-  )
+    const [tasks, setTasks] = useState<Task[]>([
+        {id: 1, title: 'React', isDone: false},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'Redux', isDone: false},
+        {id: 5, title: 'HTML', isDone: true},
+        {id: 6, title: 'CSS', isDone: false},
+    ]
+    )
+    const deleteTask = (taskId: number) => {
+        const filteredTasks = tasks.filter(task => {
+            return task.id !== taskId
+        })
+        setTasks(filteredTasks)
+    }
+    return (
+        <>
+            <div className="app">
+                <TodolistItem title="What to learn" tasks={tasks} date={'11.02.2025'} deleteTask={deleteTask}/>
+            </div>
+        </>
+    )
 }
-
-export default App
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,41 +1,44 @@
-import { Task } from './App'
-import { Button } from './Button'
+import {Task} from './App'
+import {Button} from './Button'
 
 type Props = {
-  title: string
-  tasks: Task[]
-  date?: string
+    title: string
+    tasks: Task[]
+    date?: string
+    deleteTask: (taskId: number) => void
 }
 
-export const TodolistItem = ({ title, tasks, date }: Props) => {
-  return (
-    <div>
-      <h3>{title}</h3>
-      <div>
-        <input />
-        <Button title={"+"} />
-      </div>
-      {tasks.length === 0 ? (
-        <p>Тасок нет</p>
-      ) : (
-        <ul>
-          {tasks.map(task => {
-            return (
-              <li key={task.id}>
-                <input type="checkbox" checked={task.isDone} />
-                <span>{task.title}</span>
-              </li>
-            )
-          })}
-        </ul>
-      )}
-      <div>
-        <Button title={"All"} />
-        <Button title={"Active"} />
-        <Button title={"Completed"} />
-      </div>
-      <div>{date}</div>
-    </div>
-  )
+export const TodolistItem = ({title, tasks, date, deleteTask}: Props) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <Button title={'+'}/>
+            </div>
+            {tasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <ul>
+                    {tasks.map(task => {
+                        return (
+                            <li key={task.id}>
+                                <input type="checkbox" checked={task.isDone}/>
+                                <span>{task.title}</span>
+                                <button onClick={() => deleteTask(task.id)}>+
+                                </button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
+            <div>
+                <Button title={'All'}/>
+                <Button title={'Active'}/>
+                <Button title={'Completed'}/>
+            </div>
+            <div>{date}</div>
+        </div>
+    )
 }
 
